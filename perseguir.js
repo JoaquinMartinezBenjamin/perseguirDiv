b2f4e899ff31b0c598a3e1f4e64d3ac2e9e7a299
JoaquinMartinezBenjamin <joaquin.martinez.benjamin@gmail.com>
2021-07-16 19:31:10 -0500

    first commit

---
+++ perseguir.js
@@ -0,0 +1,10 @@
+
+
+function cambia(a) {
+ <!--  a.style.top = Math.floor((Math.random() * 500) + 1) + "px";
+  a.style.right = Math.floor((Math.random() * 1000) + 1) + "px";
+   -->
+ a.style.top = Math.floor((Math.random() * 500) + 1) + "px";
+  a.style.right = Math.floor((Math.random() * 1000) + 1) + "px";
+
+}
